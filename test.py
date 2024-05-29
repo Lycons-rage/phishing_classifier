19d789e42c8cbc31fb5ae91aa55d119648226b09
Lycons-rage <chinmayatewari.20@gmail.com>
2024-05-29 11:35:24 +0530

    Frontend Prepared and requirements.txt updated | Commit 11:35 29-05-2024

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-a = {
-    'a':1,
-    'b':2,
-    'e':-9,
-    'd':5,
-    'c':9
-}
+# need to practice over a sample url for all the changes that are needed to be done
 
-max_score = max(sorted(a.values()))
-
-max_key = list(a.keys())[list(a.values()).index(max_score)]
-
-print(max_key)
